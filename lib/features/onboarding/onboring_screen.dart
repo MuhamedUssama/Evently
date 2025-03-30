a8dd1762b1b416a85e8a872fb6cdc0756986abca
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-03-30 04:31:41 +0200

    Apply theme feature on app

---
+++ lib/features/onboarding/onboring_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class OnBoringScreen extends StatelessWidget {
+  static const String routeName = '/onBoarding';
+  const OnBoringScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
