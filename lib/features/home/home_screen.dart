7dc7242eadb836943f3b12d926cdb0047220298d
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-04-02 13:36:26 +0200

    Created home screen with bottom navigation bar to navigate between tabs

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,8 +1,8 @@
-import 'package:evently/core/theme/app_theme.dart';
-import 'package:evently/core/utils/app_assets.dart';
+import 'package:evently/features/home/provider/home_screen_provider.dart';
 import 'package:evently/features/home/widgets/add_event_icon_widget.dart';
+import 'package:evently/features/home/widgets/home_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = '/home';
@@ -10,54 +10,20 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: AddEventIconWidget(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: _buildBottomNavigationBar(),
-    );
-  }
-
-  Widget _buildBottomNavigationBar() {
-    return BottomAppBar(
-      elevation: 0,
-      color: AppTheme.primary,
-      padding: EdgeInsets.zero,
-      clipBehavior: Clip.hardEdge,
-      shape: CircularNotchedRectangle(),
-      child: Theme(
-        data: ThemeData(splashColor: Colors.transparent),
-        child: BottomNavigationBar(
-          elevation: 0,
-          currentIndex: 1,
-          enableFeedback: false,
-          onTap: (value) {},
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset(AppIcons.home),
-              activeIcon: SvgPicture.asset(AppIcons.homeActive),
-              label: 'Home',
-              backgroundColor: AppTheme.primary,
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset(AppIcons.maps),
-              activeIcon: SvgPicture.asset(AppIcons.mapsActive),
-              label: 'Map',
-              backgroundColor: AppTheme.primary,
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset(AppIcons.love),
-              activeIcon: SvgPicture.asset(AppIcons.loveActive),
-              label: 'Love',
-              backgroundColor: AppTheme.primary,
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset(AppIcons.profile),
-              activeIcon: SvgPicture.asset(AppIcons.profileActive),
-              label: 'Profile',
-              backgroundColor: AppTheme.primary,
-            ),
-          ],
+    return ChangeNotifierProvider(
+      create: (context) => HomeScreenProvider(),
+      child: Scaffold(
+        body: Consumer<HomeScreenProvider>(
+          builder: (context, provider, child) {
+            return IndexedStack(
+              index: provider.currentIndex,
+              children: provider.tabs,
+            );
+          },
         ),
+        floatingActionButton: AddEventIconWidget(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: HomeBottomNavigationBar(),
       ),
     );
   }
