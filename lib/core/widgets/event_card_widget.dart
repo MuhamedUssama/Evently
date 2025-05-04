2efa8f9890b5ca4e07415ebf859931893a69d63c
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-05-04 21:06:12 +0300

    Finished home tab

--- lib/core/widgets/event_card_widget.dart
+++ lib/core/widgets/event_card_widget.dart
@@ -1,5 +1,6 @@
 import 'package:evently/core/models/event_model.dart';
 import 'package:evently/core/theme/app_theme.dart';
+import 'package:evently/core/widgets/local_cached_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
@@ -11,77 +12,79 @@ class EventCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200.h,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.r),
-        border: Border.all(color: AppTheme.primary, width: 1),
-        image: DecorationImage(
-          image: AssetImage(event.category.lightImagePath!),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.h),
-            margin: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.r),
-              color: Theme.of(
-                context,
-              ).scaffoldBackgroundColor.withValues(alpha: .8),
-            ),
-            child: Column(
-              children: [
-                Text(
-                  '${event.dateTime.day}',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                Text(
-                  DateFormat('MMM').format(event.dateTime),
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                    color: AppTheme.primary,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(16.r),
+          child: LocalCachedImage(
+            height: 200.h,
             width: double.infinity,
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            margin: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.r),
-              color: Theme.of(context).scaffoldBackgroundColor,
-            ),
-            child: Row(
-              spacing: 8,
-              children: [
-                Expanded(
-                  child: Text(
-                    event.description,
-                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.favorite_outline_sharp,
-                    color: AppTheme.primary,
-                  ),
-                ),
-              ],
-            ),
+            imagePath: event.category.lightImagePath!,
           ),
-        ],
-      ),
+        ),
+        Positioned.fill(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.all(8),
+                padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.h),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.r),
+                  color: Theme.of(
+                    context,
+                  ).scaffoldBackgroundColor.withValues(alpha: .8),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      '${event.dateTime.day}',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    Text(
+                      DateFormat('MMM').format(event.dateTime),
+                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        color: AppTheme.primary,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.all(8),
+                padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 10.h),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.r),
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                ),
+                child: Row(
+                  spacing: 8,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        event.description,
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.favorite_outline_sharp,
+                        color: AppTheme.primary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
