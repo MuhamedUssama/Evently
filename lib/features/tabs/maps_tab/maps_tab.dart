f9a3470d2e5b250e29d96afd205802803a4e7899
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-05-13 21:54:15 +0300

    Created google map ui

--- lib/features/tabs/maps_tab/maps_tab.dart
+++ lib/features/tabs/maps_tab/maps_tab.dart
@@ -1,5 +1,7 @@
+import 'package:evently/core/theme/app_theme.dart';
 import 'package:evently/features/tabs/maps_tab/provider/maps_tab_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
 class MapsTab extends StatelessWidget {
@@ -8,6 +10,30 @@ class MapsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     MapsTabProvider provider = Provider.of<MapsTabProvider>(context);
-    return Center(child: Text(provider.locationMessage));
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: provider.cameraPosition,
+              onMapCreated: (controller) {
+                provider.googleMapController = controller;
+              },
+              mapType: MapType.normal,
+              markers: provider.markers,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          provider.getLocation();
+        },
+        backgroundColor: Theme.of(context).primaryColor,
+        foregroundColor: AppTheme.backgroundLight,
+        child: Icon(Icons.gps_fixed),
+      ),
+    );
   }
 }
--- lib/features/tabs/maps_tab/provider/maps_tab_provider.dart
+++ lib/features/tabs/maps_tab/provider/maps_tab_provider.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 class MapsTabProvider extends ChangeNotifier {
   Location location = Location();
-  String locationMessage = '';
 
-  MapsTabProvider() {
-    getLocation();
-  }
+  late GoogleMapController googleMapController;
+
+  CameraPosition cameraPosition = const CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  Set<Marker> markers = {};
 
   Future<bool> _getLocationPermissioin() async {
     PermissionStatus permissionStatus;
@@ -34,26 +39,51 @@ class MapsTabProvider extends ChangeNotifier {
   Future<void> getLocation() async {
     bool permissionGranted = await _getLocationPermissioin();
     if (!permissionGranted) {
-      locationMessage = 'Location Permission denied';
-      notifyListeners();
       return;
     }
 
     bool serviceEnabled = await _checkLocationService();
     if (!serviceEnabled) {
-      locationMessage = 'Location service disabled';
-      notifyListeners();
       return;
     }
 
-    locationMessage =
-        'Location service enabled and now we are getting location';
-    notifyListeners();
-
     LocationData locationData = await location.getLocation();
 
-    locationMessage =
-        'Location: ${locationData.latitude}, ${locationData.longitude}';
+    changeLocationOnMap(locationData);
+
     notifyListeners();
+  }
+
+  void changeLocationOnMap(LocationData locationData) {
+    cameraPosition = CameraPosition(
+      target: LatLng(locationData.latitude ?? 0, locationData.longitude ?? 0),
+      zoom: 14.4746,
+    );
+
+    markers.add(
+      Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(
+          locationData.latitude ?? 0,
+          locationData.longitude ?? 0,
+        ),
+        infoWindow: InfoWindow(title: 'User Location'),
+      ),
+    );
+
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(cameraPosition),
+    );
+
+    notifyListeners();
+  }
+
+  void setLocationListener() {
+    location.changeSettings(accuracy: LocationAccuracy.high, interval: 1000);
+
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      changeLocationOnMap(currentLocation);
+      notifyListeners();
+    });
   }
 }
